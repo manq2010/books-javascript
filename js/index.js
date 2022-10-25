ea983d65c4c85fbe7542c55c98b8dffcd596e24d
manq2010 <manq2010@gmail.com>
2022-10-25 11:42:22 +0200

    Fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,79 +1,65 @@
-// Array Object
+// Array Constructor
 
-const booksObject = new Object();
-// const booksObject = new Object.create();
+const booksObject = {};
 
-// Storage
-const getBooks = () =>{
-    let books;
-    if (localStorage.getItem('books')===null){
-        books= [];
-    }else{
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    return books;
-}
+// Storage Functions
+
+const getBooks = () => {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  return books;
+};
 
 const addBookStr = (book) => {
-const books = getBooks();
-books.push(book)
-localStorage.setItem('books', JSON.stringify(books));
-}
+  const books = getBooks();
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 const deleteBookStr = (bookIndex) => {
-const books = getBooks();
-books.forEach((book, index)=>{
-    console.log(index, book)
-    if(bookIndex===index){
-        books.splice(index, 1)
-    } 
-})
-localStorage.setItem('books', JSON.stringify(books));
-}
+  const books = getBooks();
+
+  books.forEach((book, index) => {
+    if (bookIndex === index) {
+      books.splice(index, 1);
+    }
+  });
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 // Array UI
 const books = getBooks();
-// const books = [
-//     {
-//         title: 'Man',
-//         author: 'Two',
-//     },
-//     {
-//         title: 'Festus',
-//         author: 'One',
-//     }
-// ]
 
-// function: Add book
 const addBook = (book) => {
+  const list = document.querySelector('.container-books');
+  const div = document.createElement('div');
+  div.classList.add('list-container');
+  list.appendChild(div);
 
-    const list = document.querySelector('.container-books');
-    const div = document.createElement('div');
-    div.classList.add('list-container');
-    list.appendChild(div);
-
-    div.innerHTML = `
-    <h3>${book.title}</h3>
-    <p>${book.author}</p>
-    <a href="" class="btn btn-danger btn-sm remove"> Remove </a>
-    <hr>
-    `
+  div.innerHTML = `
+  <h3>${book.title}</h3>
+  <p>${book.author}</p>
+  <a href="" class="btn btn-danger btn-sm remove"> Remove </a>
+  <hr> `;
 };
 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+
 const clearFormInputs = () => {
-    title.value = '';
-    author.value = '';
-}
+  title.value = '';
+  author.value = '';
+};
 
 const deleteBookList = (element) => {
-    if (element.classList.contains('remove')) {
-        element.parentElement.remove();
-    }
-}
-
-const showAlert = () => {
-
-}
+  if (element.classList.contains('remove')) {
+    element.parentElement.remove();
+  }
+};
 
 // Events: Display Books
 
@@ -82,43 +68,36 @@ books.forEach((book) => addBook(book));
 // Event: add a book
 
 const formBook = document.querySelector('#book-form');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
 
 formBook.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
+  booksObject.title = title.value;
+  booksObject.author = author.value;
 
-    booksObject.title = title.value;
-    booksObject.author = author.value;
+  // validation and Alerts
+  if (title.value === '' || author.value === '') {
+    //
+  } else {
+    // add book to defined array
+    addBook(booksObject);
 
-    // validation and Alerts
+    // add book to storage
+    addBookStr(booksObject);
+    // clear form
+    clearFormInputs();
+  }
 
-    if (title.value === '' || author.value === '') {
-        alert('please fill in missing details');
-
-    } else {
-        // add book to defined array
-        addBook(booksObject);
-        //add book to storage
-        addBookStr(booksObject);
-        // clear form
-        clearFormInputs();
-    }
-
-})
+  window.location.reload();
+});
 
 // remove a book
 
 const remove = document.querySelectorAll('.list-container');
 
 remove.forEach((deleteBook, index) => {
-
-    deleteBook.addEventListener('click', (e) => {
-        e.preventDefault();
-        // console.log(e.target.classList.contains('remove'))
-
-        deleteBookList(e.target);
-        deleteBookStr(index)
-    })
+  deleteBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteBookList(e.target);
+    deleteBookStr(index);
+  });
 });
-
